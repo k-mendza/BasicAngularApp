5979414b110f2f6e445951c24cbec2f2f8d6cf0b
k-mendza <karolmendza@gmail.com>
2019-05-04 12:36:26 +0200

    Added property binding

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,13 +1,13 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-    selector: "app-server",
-    templateUrl: "./server.component.html",
-    styleUrls: ["./server.component.css"]
+    selector: 'app-server',
+    templateUrl: './server.component.html',
+    styleUrls: ['./server.component.css']
 })
 export class ServerComponent implements OnInit {
     serverId = 10;
-    serverStatus = "offline";
+    serverStatus = 'offline';
     constructor() {}
 
     ngOnInit() {}
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-servers',
-  templateUrl: './servers.component.html',
-  styleUrls: ['./servers.component.css']
+    selector: 'app-servers',
+    templateUrl: './servers.component.html',
+    styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+    allowNewServers = false;
 
-  constructor() { }
+    constructor() {
+        setTimeout(() => { this.allowNewServers = true; }, 2000);
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
 }
