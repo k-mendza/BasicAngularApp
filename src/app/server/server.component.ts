c0a0d54a952a723c812e27c4b0c6467f6633c5b9
k-mendza <karolmendza@gmail.com>
2019-05-04 12:23:52 +0200

    Added databinding for serverComponent via string interpolation

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-server',
-  templateUrl: './server.component.html',
-  styleUrls: ['./server.component.css']
+    selector: "app-server",
+    templateUrl: "./server.component.html",
+    styleUrls: ["./server.component.css"]
 })
 export class ServerComponent implements OnInit {
+    serverId = 10;
+    serverStatus = "offline";
+    constructor() {}
 
-  constructor() { }
+    ngOnInit() {}
 
-  ngOnInit() {
-  }
-
+    getServerStatus() {
+        return this.serverStatus;
+    }
 }
