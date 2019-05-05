0d6b9578aefc0fd8dfd85efe11dc659544de277d
k-mendza <karolmendza@gmail.com>
2019-05-05 21:05:22 +0200

    Added ngStyle directive

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -8,11 +8,17 @@ import { Component, OnInit } from '@angular/core';
 export class ServerComponent implements OnInit {
     serverId = 10;
     serverStatus = 'offline';
-    constructor() {}
+    constructor() {
+        this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+    }
 
     ngOnInit() {}
 
     getServerStatus() {
         return this.serverStatus;
+    }
+
+    getColor() {
+        return this.serverStatus === 'online' ? 'green' : 'red';
     }
 }
